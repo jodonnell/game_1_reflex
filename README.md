4857ad41a3f71e58c7fcff30d6150ce16c61c49e
Jacob O'Donnell <jacobodonnell@gmail.com>
2020-04-18 15:42:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Game Challenge
+Jacob and Mark trade taking charge of making a game within a timebox.  The first game each of us has one hour.  Game 2 has 2 hours each and progressively longer until we get bored.
+
+# Game 1 - REFLEX
+
+Initial Idea: Some sort of queue happens and the faster you hit space bar the more points you get.  Objective is to get as many points as you can in a round.
---
+++ game1.js
@@ -0,0 +1,67 @@
+function keydown(event) {
+    if (event.keyCode === 32) {
+        console.log('here')
+    }
+}
+
+document.addEventListener('keydown', keydown, false);
+
+
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const rect = canvas.getBoundingClientRect();
+
+
+let squareCenter = null;
+let randomX = null;
+let randomY = null;
+let size = 5;
+let counter = 0;
+let points = 0;
+const canvasWidth = 800;
+const canvasHeight = 600;
+let gameOver = false;
+
+
+(function animationLoop(){
+    if (!gameOver)
+        window.requestAnimationFrame(animationLoop);
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    ctx.fillStyle = 'white';
+    ctx.font = "30px Arial";
+    ctx.fillText(`Points ${points}`, 10, 50);
+
+    if (randomX === null) {
+        randomX = Math.floor(Math.random() * 600 + 100); // 100-700
+        randomY = Math.floor(Math.random() * 400 + 100); // 100-500
+
+    }
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(randomX, randomY, size, size);
+
+    counter++;
+
+    if ((counter % 3) === 0)
+        size++;
+})();
+
+
+canvas.addEventListener('mousedown', e => {
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const xMatches = (x > randomX) && (x < randomX + size);
+    const yMatches = (y > randomY) && (y < randomY + size);
+    if (xMatches && yMatches) {
+        points += Math.max(105 - size, 0);
+        size = 5;
+        randomX = null;
+        randomY = null;
+    } else {
+        gameOver = true;
+    }
+});
